436ba3f3234e5a5137995bd68f76788036f0c593
Florian L <florian@e-lam.net>
2013-09-30 00:48:41 +0200

    Add method_missing for User

--- lib/trakt/user.rb
+++ lib/trakt/user.rb
@@ -14,5 +14,12 @@ module Trakt
       get_with_args('/user/library/shows/collection.json/', *args)
     end
 
+    def method_missing(m, *args, &block)
+      actions = m.split(/_/)
+      if actions[0] == 'get'
+        get_with_args("/user/#{actions[1..-1].join('/')}.json/", *args)
+      end
+    end
+
   end
 end
